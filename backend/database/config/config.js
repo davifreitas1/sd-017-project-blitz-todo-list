5a10e178d4a895ceab5fe17d7e5ea0ca27c2f44f
unknown <davivarejao@gmail.com>
2022-07-04 20:02:12 -0300

    altera config do sequelize, cria .env e cria arquivo docker compose básico

---
+++ backend/database/config/config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const options = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
+  database: process.env.MYSQL_DB_NAME,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: {
+    ...options,
+  },
+};
