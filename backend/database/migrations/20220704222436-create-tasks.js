8e4a8b6d0d2483b218d41e318cec8d7454b4f32f
unknown <davivarejao@gmail.com>
2022-07-04 19:27:47 -0300

    cria migrations users e tasks

---
+++ backend/database/migrations/20220704222436-create-tasks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      task: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
